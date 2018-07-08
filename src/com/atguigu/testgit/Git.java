f28a87e3753562844e7341f3f3ebac25107922b0
liuyi <liulull002@126.com>
2018-07-08 22:56:42 +0800

    更新2

--- src/com/atguigu/testgit/Git.java
+++ src/com/atguigu/testgit/Git.java
@@ -3,5 +3,7 @@ package com.atguigu.testgit;
 public class Git {
 	public static void main(String[] args) {
 		System.out.println("eclipse的git");
+		System.out.println("eclipse的git");
+		System.out.println("eclipse的git");
 	}
 }
